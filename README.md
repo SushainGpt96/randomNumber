6cd66b58c1775c39c56ff7b0ef457fd8f67c1686
SushainGpt96 <89188249+SushainGpt96@users.noreply.github.com>
2021-08-27 17:33:22 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -5,14 +5,19 @@ This Package returns a Random Number from 0-9.
 ## Installation
 
 ```
-$ npm i sushain-random0-9
+$ npm i sushain-random-number
 ```
 
 ## Usage
 
 ```
 
-const random = require('sushain-random0-9')
-console.log(random())
+const random = require('sushain-random-number')
+
+Can directly call getRandom() // Will return 10 digit Random Number.
+
+Can define range e.g. getRandom(1,10000) // Will return number between defined range.
+
+If single digit is entered e.g. getRandom(5) // Will return 5 digit Number
 
 ```
--- random.js
+++ random.js
@@ -1,3 +1,17 @@
-module.exports = () => {
-  return Date.now().toString().split("").reverse()[1];
+const getRandom = (n1 = 10, n2) => {
+  let value = new Date().getMilliseconds();
+  let array = [];
+  let output;
+  if (!n2) {
+    for (let i = 1; i <= n1; i++) {
+      let value2 = value * i;
+      array.push(value2);
+      output = array.join("");
+    }
+    console.log(output.substring(0, n1));
+  } else {
+    let value2 = parseInt((value / 1000) * (n2 - n1) + n1);
+    console.log(value2);
+  }
 };
+module.exports = getRandom;
